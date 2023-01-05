1c444de7287912ba901ffd9e8cebb44e1a4016c0
AlexisKcc8 <CodeKJS863@gmail.com>
2023-01-04 22:16:02 -0600

    adicion de scripts js  de bootstrap - (ejecutar npm install al bajar los cambios)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+//? Decorador del componente principal
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Proyecto-Parcial1';
